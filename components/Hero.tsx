75d481b7c3cc011f6e7807083bc0ec0f81505789
merve <merve@MERVEA>
2024-08-01 00:57:23 +0300

    First Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from 'react-icons/fa';
+import MagicButton from './ui/MagicButton';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 
@@ -22,13 +24,21 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Consepts into seamless Experiences"
+            words="Transforming Consepts into seamless User Experiences"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Merve, a Front End Developer
           </p>
 
+          <a href='#about'>
+            <MagicButton
+            title = "Show my work"
+            icon = {<FaLocationArrow />}
+            position='right'
+            />
+
+          </a>
 
         </div>
       </div>
