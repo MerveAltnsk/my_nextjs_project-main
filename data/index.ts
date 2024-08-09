3949faed63f36137929188238e76fb10d6139c1f
merve <merve@MERVEA>
2024-08-09 20:57:40 +0300

    Cards design in home page

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+export const gridItems = [
+    { title: 'Title1', description: 'Description', id: 1},
+    { title: 'Title2', description: 'Description', id: 2},
+    { title: 'Title3', description: 'Description', id: 3},
+    { title: 'Title4', description: 'Description', id: 4},
+    { title: 'Title5', description: 'Description', id: 5},
+    { title: 'Title6', description: 'Description', id: 6},
+  ]
